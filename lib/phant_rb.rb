c6638e3c00cc68e56040def57cef212674f8acfb
David Kitcher-Jones <m4ddav3@users.noreply.github.com>
2016-05-09 13:36:25 +0100

    Pull fields from the Phant streams info

--- lib/phant_rb.rb
+++ lib/phant_rb.rb
@@ -5,18 +5,36 @@ require 'hashie'
 
 module PhantRb
   class Client
-    def initialize(public_key, fields, opts = {})
+    def initialize(public_key, opts = {})
       @opts = {
         base_url: 'http://data.sparkfun.com/'
       }.merge(opts)
-      @fields = fields
       @public_key = public_key
+      
+      stream_info = stream()
+      
+      if stream_info.stream.has_key?('_doc')
+        details = stream_info.stream._doc
+      elsif stream_info.stream.has_key?('fields')
+        details = stream_info.stream
+      end
+      
+      @title       = details.title
+      @description = details.description
+      @fields      = details.fields
+      @created     = details.date
     end
 
     def log(*data)
       conn = rest_conn 'input'
       @last_response = conn.post URI.encode_www_form(@fields.zip(data))
       Hashie::Mash.new(JSON.parse(@last_response.body))
+    end
+
+    def stream
+      conn = rest_conn 'streams'
+      response = conn.get
+      Hashie::Mash.new(JSON.parse(response.body))
     end
 
     def get(params = {})
